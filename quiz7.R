65660ea70be29a3066d648ab0a60871df0f1455b
apag101 <apag101@yahoo.com>
2014-10-12 13:53:01 -0400

    Quiz 7

---
+++ quiz7.R
@@ -0,0 +1,25 @@
+#Regular Expressions
+#Regular expressions are best used for data that is using different formats. 
+#For example, if you had 2 files where one file uses 3/1/14 as the date format 
+#and the other file uses March 1, 2014 as date in one part of the file and 3-1-14
+#another part of the file, it would be a good candidate for regular expresssion.
+#In this scenerio we would use the search method of Regular Expressions to search
+# for specific patterns in each file using wildcards.
+
+#R Markdown
+#R Markdown is an example of reproducible research because it is a tool that provides
+#a way to use R code inside a HTML type presentation. It uses easy to learn syntaxt
+#while allowing R code to be written in its native form. THis tools allows coders to 
+#reproduce their code quickly and easily in a pdf, slideshow, html, word formats and more.
+
+#Scatterplots and Correlation
+#Correlation shows how 2 variables relate to one another. They can either have a negative
+#correlation where one goes the opposite of the other or a positive correlation where they 
+#both go the same direction or no correlation. Causation states that although to variables correlate
+#doesn't necessarily mean one variablle would cause or effect the other.
+
+#Variable Coding
+#Variable coding helps categorise data with more descriptive column naming. Maybe you have have voters
+#broken into two parties, Republicans and Democrats. If has location information, you can add another 
+#demension such as state and add a more descriptive name by state (EG. NY, CT, NJ) to get a more detailed
+#overview of which specific states lean more towards Democrat or Republican.
