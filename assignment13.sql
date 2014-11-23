d88538fe42bbe758884148c2b137c6759ec8172d
apag101 <apag101@yahoo.com>
2014-11-23 16:09:22 -0500

    week 13

---
+++ assignment13.sql
@@ -0,0 +1,18 @@
+﻿--IS 360 Week 13 Assignment Page 1
+--Week 13 Assignment
+--Week 13 assignment is due end of day on Tuesday November 25th. You may work with a partner on the assignment.
+--Please use the tables in the flights database. Directions for downloading and restoring the flights database are provided in the Week 10 folder. Your deliverable is the SQL query or queries.
+--A solution to the assignment will be posted on Wednesday November 26th.
+--1. Show every possible combination of airline and airport in the flights database.
+
+
+select a.name as airlines, p.name as airports from airlines a, airports p order by airlines, airports
+
+
+--2. For every airport north of Fairbanks, show the name of the airport, and all airports north of that airport. The resultset below sorted first by the name of the airport, then by the most northerly “more northerly” airports first.
+
+select a.name, a.lat, b.name, b.lat from airports a join airports b on a.faa = b.faa
+where a.lat > (select lat from airports where name like 'Fairbanks%') or b.lat > a.lat
+order by a.name;
+
+--Challenge question. Can you find an obvious outlier in this dataset?i How could you rewrite the query to exclude the outlier airport? What would be another approach to handling this outlier?
---
+++ quiz13.sql
@@ -0,0 +1,23 @@
+﻿
+--IS 360 Week 13 Quiz Page 1
+--Week 13 Quiz
+--The Week 13 quiz is due end of day Sunday November 23rd. You may work with a partner if you wish.
+--Suppose you are tasked with creating an HTML form that displays all of the combinations of information from two SQL tables for a language school. The first table has the world’s four most popular “mother tongue” languages.i The second table has a list of courses: “Beginning”, “Intermediate” and “Advanced.” Assume that the first table is called languages, and the second table is called courselevels, and that both columns keep the descriptive text in a column called “name.” Write a SQL query that returns a single column of information for every combination, such as “Intermediate Hindi.”
+--Mockup of sample HTML form constructed by combining information from all of the rows in two SQL tables.
+
+SELECT 
+ concat("Courselevels".courselevel, ' ',"Language"."Language")
+FROM 
+  public."Language", 
+  public."Courselevels";
+
+--Challenge exercise (Successful completion of this exercise may require working a week ahead in the course material, by reading The Language of SQL, chapters 17 and 18):
+--A. Create and populate the two tables needed to generate this information.
+--B. What else do you need to do to be sure that the most popular “mother tongue” languages are listed first, and that they are always listed in the order of Beginning, Intermediate, then Advanced?ii
+
+SELECT 
+ concat("Courselevels".courselevel, ' ',"Language"."Language")
+FROM 
+  public."Language", 
+  public."Courselevels"
+  order by "Language"."Language", "Courselevels".courselevel;
